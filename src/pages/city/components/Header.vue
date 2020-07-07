59336d1eccec55f70a1ec2fa24bb31ea988dc9ff
withtimesgo1115 <channingliu1115@gmail.com>
2020-07-07 11:31:18 +0200

    add search to city

--- src/pages/city/components/Header.vue
+++ src/pages/city/components/Header.vue
@@ -23,13 +23,13 @@ export default {
     text-align: center
     color: #fff
     background: $bgColor
-    font-size: .32 rem
+    font-size: .32rem
     .header-back
       position: absolute
       top: 0
       left: 0
-      width: .64 rem
+      width: .64rem
       text-align: center
-      font-size: .4 rem
+      font-size: .4rem
       color: #fff
 </style>
