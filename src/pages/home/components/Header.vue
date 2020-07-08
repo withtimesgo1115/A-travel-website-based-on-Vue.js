8905f13da6b351a6ca6b2d1f82bc0ed025ad15f9
withtimesgo1115 <channingliu1115@gmail.com>
2020-07-08 10:58:45 +0200

    use vuex

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -7,18 +7,20 @@
         <span class="iconfont">&#xe632;</span>
         输入城市/景点/游玩主题</div>
     <router-link to='/city'>
-      <div class="header-right arrow-icon">{{this.city}}
-        <span class="iconfont">&#xe64a;</span>
+      <div class="header-right">
+        {{this.city}}
+        <span class="iconfont arrow-icon">&#xe64a;</span>
       </div>
     </router-link>
 </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 export default {
   name: 'HomeHeader',
-  props: {
-    city: String
+  computed: {
+    ...mapState(['city'])
   }
 }
 </script>
@@ -47,7 +49,8 @@ export default {
       border-radius: .1rem
       color: #ccc
     .header-right
-      width: 1.24rem
+      min-width: 1.04rem
+      padding: 0 .1rem
       float: right
       text-align: center
       color: #fff
